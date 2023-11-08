40c17a14f74a87b96ce6e7b6e371b1554df496d4
Jkwashu <WashumJk@hendrix.edu>
2023-11-07 23:48:12 -0600

    update for new pc to clone

--- RazorPagesMovie/Models/Movie.cs
+++ RazorPagesMovie/Models/Movie.cs
@@ -1,12 +1,11 @@
-using System.CompenentModel.DataAnnotations;
+
 
 namespace RazorPagesMovie.Models;
 
-public class Games
+public class Movie
 {
     public int Id { get; set; }
     public string? Title { get; set; }
-    [DataType(DataType.Date)]
     public DateTime ReleaseDate { get; set; }
     public string? Genre {get; set; }
     public decimal Price { get; set; }
